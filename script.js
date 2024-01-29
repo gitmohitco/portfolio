0bfefb51c6c77eea1033b5a2f68ed9973aefc80b
gitmohitco <152581299+gitmohitco@users.noreply.github.com>
2024-01-29 10:59:10 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    })
+})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el))
+const hiddenLogo = document.querySelectorAll('.logo');
+hiddenLogo.forEach((el)=> observer.observe(el))
+const hiddenSk = document.querySelectorAll('.sk');
+hiddenSk.forEach((el)=> observer.observe(el))
+const hiddenAbout = document.querySelectorAll('.abbox');
+hiddenAbout.forEach((el)=> observer.observe(el))
+const hiddenText = document.querySelectorAll('.text');
+hiddenText.forEach((el)=> observer.observe(el))
+
+const java = document.querySelector(".java-description");
+document.getElementById("java").addEventListener('click',()=>{
+    java.style.display = "block";
+})
+const cls = document.querySelector('.close');
+        cls.addEventListener('click',()=>{
+            java.style.display = "none";
+})
